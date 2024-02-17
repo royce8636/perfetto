9afec27c105f91998e06da0fdd6908987536616e
Jaeheon Lee <royce8636@gmail.com>
2024-02-17 18:45:52 +0900

    ui: working import

--- ui/src/common/rtux_common.ts
+++ ui/src/common/rtux_common.ts
@@ -29,7 +29,6 @@ function readRtuxFile(file: File): Promise<Array<{ key: number, value: string }>
     });
 }
 
-
 // A new function to access the stored vector
 function getStoredVector(): Array<{ key: number, value: string }> {
     return globalVector;
@@ -57,38 +56,3 @@ export const rtux_common = {
     getStoredVector, // Allow access to the stored vector
     getTimestamps,
 };
-
-
-// function readRtuxFile(file: File): Promise<Array<{key: number, value: string}>> {
-//     return new Promise((resolve, reject) => {
-//       const reader = new FileReader();
-//       reader.onload = () => {
-//         // Convert the result to a string
-//         const str = reader.result as string;
-  
-//         // Parse the string into a vector
-//         const lines = str.split('\n');
-//         const vector = lines.map(line => {
-//           const [key, value] = line.split(': ');
-//           return {key: parseFloat(key), value};
-//         });
-  
-//         resolve(vector);
-//       };
-//       reader.onerror = () => {
-//         reject(reader.error);
-//       };
-//       reader.readAsText(file);
-//     });
-//   }
-
-//   function openRtuxFromFile(file: File) {
-//     readRtuxFile(file).then(vector => {
-//       return vector;
-//     });
-//   }
-  
-//   export const RTUX_common = {
-//     readRtuxFile,
-//     openRtuxFromFile,
-//   };
